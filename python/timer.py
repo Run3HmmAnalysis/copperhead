6da6babef34cd294f0ba91e6f6d28442a0938236
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2020-01-28 11:11:14 -0500

    add missing imports

--- python/timer.py
+++ python/timer.py
@@ -1,11 +1,15 @@
+import time
+import pandas as pd
+import numpy as np
+
 class Timer(object):
     def __init__(self, name="t"):
-        import time
         self.name = name
         self.time_dict = {}
         self.last_checkpoint = time.time()    
         
     def update(self):
+        import time
         self.last_checkpoint = time.time()
         
     def add_checkpoint(self, comment):
@@ -20,7 +24,7 @@ class Timer(object):
     def summary(self):
         columns = ["Action", "Time (s)", "% CPU"]
         summary = pd.DataFrame(columns=columns)        
-        total_time = sum(list(self.time_dict.values()))
+        total_time = round(sum(list(self.time_dict.values())),5)
         summary[columns[0]] = np.array(list(self.time_dict.keys()))
         summary[columns[1]] = np.round( np.array(list(self.time_dict.values())), 5)
         summary[columns[2]] = np.round( 100*np.array(list(self.time_dict.values()))/total_time, 3)
@@ -30,7 +34,7 @@ class Timer(object):
         print('-'*50)
         print(summary)
         print('-'*50)
-        print(f'Total time: {total_time}')
+        print(f'Total time: {total_time} s')
         print('='*50)
         print()
         
