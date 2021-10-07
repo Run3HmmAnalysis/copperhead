45250f2670ecca9c17a0cba0ee1a43aa97ec369b
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2021-10-07 13:24:54 -0400

    move binning to separate file

--- post_dask.py
+++ post_dask.py
@@ -1,9 +1,11 @@
+import glob
 import dask
 from dask.distributed import Client
 
 from python.timer import Timer
-from python.postprocessor import workflow, plotter, grouping, grouping_alt
-import glob
+from python.postprocessor import workflow, plotter, grouping_alt
+# from python.postprocessor import grouping
+from mva_bins import mva_bins
 
 __all__ = ["dask"]
 
@@ -53,109 +55,6 @@ parameters = {
     "channels": ["vbf", "vbf_01j", "vbf_2j"],
     # 'channels': ['ggh_01j', 'ggh_2j'],
     "regions": ["h-peak", "h-sidebands"],
-}
-
-mva_bins = {
-    "dnn_allyears_128_64_32": {
-        "2016": [
-            0,
-            0.16,
-            0.374,
-            0.569,
-            0.75,
-            0.918,
-            1.079,
-            1.23,
-            1.373,
-            1.514,
-            1.651,
-            1.784,
-            1.923,
-            2.8,
-        ],
-        "2017": [
-            0,
-            0.307,
-            0.604,
-            0.832,
-            1.014,
-            1.169,
-            1.31,
-            1.44,
-            1.567,
-            1.686,
-            1.795,
-            1.902,
-            2.009,
-            2.8,
-        ],
-        "2018": [
-            0,
-            0.07,
-            0.432,
-            0.71,
-            0.926,
-            1.114,
-            1.28,
-            1.428,
-            1.564,
-            1.686,
-            1.798,
-            1.9,
-            2.0,
-            2.8,
-        ],
-    },
-    "bdt_nest10000_weightCorrAndShuffle_2Aug": {
-        "2016": [
-            0,
-            0.282,
-            0.57,
-            0.802,
-            0.999,
-            1.171,
-            1.328,
-            1.479,
-            1.624,
-            1.775,
-            1.93,
-            2.097,
-            2.288,
-            5.0,
-        ],
-        "2017": [
-            0,
-            0.411,
-            0.744,
-            0.99,
-            1.185,
-            1.352,
-            1.504,
-            1.642,
-            1.784,
-            1.924,
-            2.07,
-            2.222,
-            2.398,
-            5.0,
-        ],
-        "2018": [
-            0,
-            0.129,
-            0.621,
-            0.948,
-            1.189,
-            1.388,
-            1.558,
-            1.717,
-            1.866,
-            2.01,
-            2.152,
-            2.294,
-            2.451,
-            5.0,
-        ],
-    },
 }
 
 parameters["mva_bins"] = mva_bins
