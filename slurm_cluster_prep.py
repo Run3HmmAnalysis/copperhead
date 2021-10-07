d86d46f3c787ad52b484c277bfd12d61cd8116a1
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2021-10-07 14:02:29 -0400

    update script

--- slurm_cluster_prep.py
+++ slurm_cluster_prep.py
@@ -4,23 +4,33 @@ import dask
 from dask.distributed import Client
 from dask.distributed import Scheduler, Worker
 from dask_jobqueue import SLURMCluster
-from coffea.processor.executor import dask_executor
+from coffea.processor import dask_executor
 from python.processor import DimuonProcessor
+
 dask.config.set({"temporary-directory": "/tmp/dask-temp/"})
-dask.config.set({'distributed.worker.timeouts.connect': '60s'})
+dask.config.set({"distributed.worker.timeouts.connect": "60s"})
 
-__all__ = ['pytest', 'asyncio', 'dask',
-           'Client', 'Scheduler', 'Worker',
-           'SLURMCluster', 'dask_executor',
-           'DimuonProcessor']
+__all__ = [
+    "pytest",
+    "asyncio",
+    "dask",
+    "Client",
+    "Scheduler",
+    "Worker",
+    "SLURMCluster",
+    "dask_executor",
+    "DimuonProcessor",
+]
 
-print('Dask version:', dask.__version__)
+print("Dask version:", dask.__version__)
 
 
 async def f(scheduler_address):
-    r = await Worker(scheduler_address,
-                     resources={'processor': 0, 'reducer': 1},
-                     ncores=1,
-                     nthreads=1,
-                     memory_limit='64GB')
+    r = await Worker(
+        scheduler_address,
+        resources={"processor": 0, "reducer": 1},
+        ncores=1,
+        nthreads=1,
+        memory_limit="64GB",
+    )
     await r.finished()
