9c012166b96dfbe6b535db4341c4c9b5de8b6379
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2021-10-13 10:52:25 -0400

    paths & cross sections

--- delphes/cross_sections.py
+++ delphes/cross_sections.py
@@ -1,4 +1,30 @@
 cross_sections = {
-    # 13 TeV value - to be updated
-    "ggh_powheg": 0.01057
+    # signal
+    "ggh_powheg": 0,
+    "vbf_powheg": 0,
+    # drell-yan
+    "dy_m100_mg": 246.5,
+    # ttbar
+    "ttbar_dl": 67.75,
+    "ttbar_sl": 0,
+    "tttj": 0.000474,
+    "tttt": 0.01206,
+    "tttw": 0.0008667,
+    "ttwj": 0.2253,
+    "ttww": 0.00788,
+    "ttz": 0.2253,
+    # single top
+    "st_s": 3.71,
+    "st_t_top": 48.03,
+    "st_t_antitop": 29.2,
+    "st_tw_top": 45.02,
+    "st_tw_antitop": 45.02,
+    "tzq": 0,
+    # vv
+    "zz_2l2q": 3.721,
+    # vvv
+    "www": 0.2362,
+    "wwz": 0.1889,
+    "wzz": 0.06376,
+    "zzz": 0.0158,
 }
