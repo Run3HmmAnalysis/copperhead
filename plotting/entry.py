f8610b19d6970d4d8201a3f320c8a730275838c6
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2021-10-15 16:48:25 -0400

    more cleanup for plotting code

---
+++ plotting/entry.py
@@ -0,0 +1,91 @@
+import pandas as pd
+
+
+class Entry(object):
+    """
+    Different types of entries to put on the plot
+    """
+
+    def __init__(self, entry_type="step", parameters={}):
+        self.entry_type = entry_type
+        self.labels = []
+        if entry_type == "step":
+            self.groups = parameters["step_groups"]
+            self.histtype = "step"
+            self.stack = False
+            self.plot_opts = {"linewidth": 3}
+            self.yerr = False
+        elif entry_type == "stack":
+            self.groups = parameters["stack_groups"]
+            self.histtype = "fill"
+            self.stack = True
+            self.plot_opts = {"alpha": 0.8, "edgecolor": (0, 0, 0)}
+            self.yerr = False
+        elif entry_type == "data":
+            self.groups = parameters["data_groups"]
+            self.histtype = "errorbar"
+            self.stack = False
+            self.plot_opts = {"color": "k", "marker": ".", "markersize": 10}
+            self.yerr = True
+        else:
+            raise Exception(f"Wrong entry type: {entry_type}")
+
+        self.entry_dict = {
+            e: g
+            for e, g in parameters["grouping"].items()
+            if (g in self.groups) and (e in parameters["datasets"])
+        }
+        self.entry_list = self.entry_dict.keys()
+        self.labels = self.entry_dict.values()
+        self.groups = list(set(self.entry_dict.values()))
+
+    def get_plottables(self, hist, year, var_name, dimensions):
+        slicer_value = tuple(list(dimensions) + ["value", slice(None)])
+        slicer_sumw2 = tuple(list(dimensions) + ["sumw2", slice(None)])
+        plottables_df = pd.DataFrame(columns=["label", "hist", "sumw2", "integral"])
+        all_df = pd.DataFrame(columns=["label", "integral"])
+        for group in self.groups:
+            group_entries = [e for e, g in self.entry_dict.items() if (group == g)]
+            all_labels = hist.loc[hist.dataset.isin(group_entries), "dataset"].values
+            hist_values_group = [
+                hist[slicer_value].project(var_name)
+                for hist in hist.loc[hist.dataset.isin(group_entries), "hist"].values
+            ]
+            hist_sumw2_group = [
+                hist[slicer_sumw2].project(var_name)
+                for hist in hist.loc[hist.dataset.isin(group_entries), "hist"].values
+            ]
+            if len(hist_values_group) == 0:
+                continue
+            nevts = sum(hist_values_group).sum()
+
+            if nevts > 0:
+                plottables_df = plottables_df.append(
+                    pd.DataFrame(
+                        [
+                            {
+                                "label": group,
+                                "hist": sum(hist_values_group),
+                                "sumw2": sum(hist_sumw2_group),
+                                "integral": sum(hist_values_group).sum(),
+                            }
+                        ]
+                    ),
+                    ignore_index=True,
+                )
+            for label, h in zip(all_labels, hist_values_group):
+                all_df = all_df.append(
+                    pd.DataFrame(
+                        [
+                            {
+                                "label": label,
+                                "integral": h.sum(),
+                            }
+                        ]
+                    ),
+                    ignore_index=True,
+                )
+        plottables_df.sort_values(by="integral", inplace=True)
+        all_df.sort_values(by="integral", inplace=True)
+        # print(all_df)
+        return plottables_df
--- plotting/plotter.py
+++ plotting/plotter.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 from hist.intervals import poisson_interval
 from python.utils import load_histograms
+from plotting.entry import Entry
 
 import matplotlib.pyplot as plt
 import mplhep as hep
@@ -12,10 +13,21 @@ style["mathtext.fontset"] = "cm"
 style["mathtext.default"] = "rm"
 plt.style.use(style)
 
+stat_err_opts = {
+    "step": "post",
+    "label": "Stat. unc.",
+    "hatch": "//////",
+    "facecolor": "none",
+    "edgecolor": (0, 0, 0, 0.5),
+    "linewidth": 0,
+}
+ratio_err_opts = {"step": "post", "facecolor": (0, 0, 0, 0.3), "linewidth": 0}
+
 
 def plotter(client, parameters, hist_df=None, timer=None):
+
+    # Load saved histograms
     if hist_df is None:
-        # Load histograms
         argsets = []
         for year in parameters["years"]:
             for var_name in parameters["hist_vars"]:
@@ -30,7 +42,6 @@ def plotter(client, parameters, hist_df=None, timer=None):
         hist_df = pd.concat(hist_rows).reset_index(drop=True)
 
     hists_to_plot = []
-    keys = []
     for year in parameters["years"]:
         for var_name in hist_df.var_name.unique():
             if var_name not in parameters["plot_vars"]:
@@ -38,16 +49,9 @@ def plotter(client, parameters, hist_df=None, timer=None):
             hists_to_plot.append(
                 hist_df.loc[(hist_df.var_name == var_name) & (hist_df.year == year)]
             )
-            keys.append(f"plot: {year} {var_name}")
-    if timer:
-        timer.add_checkpoint("Loading histograms")
 
-    plot_futures = client.map(
-        partial(plot, parameters=parameters), hists_to_plot, key=keys
-    )
+    plot_futures = client.map(partial(plot, parameters=parameters), hists_to_plot)
     yields = client.gather(plot_futures)
-    if timer:
-        timer.add_checkpoint("Plotting")
 
     return yields
 
@@ -69,115 +73,6 @@ def plot(hist, parameters={}):
             f"Histograms for more than one year provided. Will make plots only for {years[0]}."
         )
     year = years[0]
-
-    stack_groups = parameters["stack_groups"]
-    data_groups = parameters["data_groups"]
-    step_groups = parameters["step_groups"]
-
-    class Entry(object):
-        """
-        Different types of entries to put on the plot
-        """
-
-        def __init__(self, entry_type="step", parameters={}):
-            self.entry_type = entry_type
-            self.labels = []
-            if entry_type == "step":
-                self.groups = step_groups
-                self.histtype = "step"
-                self.stack = False
-                self.plot_opts = {"linewidth": 3}
-                self.yerr = False
-            elif entry_type == "stack":
-                self.groups = stack_groups
-                self.histtype = "fill"
-                self.stack = True
-                self.plot_opts = {"alpha": 0.8, "edgecolor": (0, 0, 0)}
-                self.yerr = False
-            elif entry_type == "data":
-                self.groups = data_groups
-                self.histtype = "errorbar"
-                self.stack = False
-                self.plot_opts = {"color": "k", "marker": ".", "markersize": 10}
-                self.yerr = True
-            else:
-                raise Exception(f"Wrong entry type: {entry_type}")
-
-            self.entry_dict = {
-                e: g
-                for e, g in parameters["grouping"].items()
-                if (g in self.groups) and (e in parameters["datasets"])
-            }
-            self.entry_list = self.entry_dict.keys()
-            self.labels = self.entry_dict.values()
-            self.groups = list(set(self.entry_dict.values()))
-
-        def get_plottables(self, hist, year, var_name, dimensions):
-            slicer_value = tuple(list(dimensions) + ["value", slice(None)])
-            slicer_sumw2 = tuple(list(dimensions) + ["sumw2", slice(None)])
-            plottables_df = pd.DataFrame(columns=["label", "hist", "sumw2", "integral"])
-            all_df = pd.DataFrame(columns=["label", "integral"])
-            for group in self.groups:
-                group_entries = [e for e, g in self.entry_dict.items() if (group == g)]
-                all_labels = hist.loc[
-                    hist.dataset.isin(group_entries), "dataset"
-                ].values
-                hist_values_group = [
-                    hist[slicer_value].project(var_name)
-                    for hist in hist.loc[
-                        hist.dataset.isin(group_entries), "hist"
-                    ].values
-                ]
-                hist_sumw2_group = [
-                    hist[slicer_sumw2].project(var_name)
-                    for hist in hist.loc[
-                        hist.dataset.isin(group_entries), "hist"
-                    ].values
-                ]
-                if len(hist_values_group) == 0:
-                    continue
-                nevts = sum(hist_values_group).sum()
-
-                if nevts > 0:
-                    plottables_df = plottables_df.append(
-                        pd.DataFrame(
-                            [
-                                {
-                                    "label": group,
-                                    "hist": sum(hist_values_group),
-                                    "sumw2": sum(hist_sumw2_group),
-                                    "integral": sum(hist_values_group).sum(),
-                                }
-                            ]
-                        ),
-                        ignore_index=True,
-                    )
-                for label, h in zip(all_labels, hist_values_group):
-                    all_df = all_df.append(
-                        pd.DataFrame(
-                            [
-                                {
-                                    "label": label,
-                                    "integral": h.sum(),
-                                }
-                            ]
-                        ),
-                        ignore_index=True,
-                    )
-            plottables_df.sort_values(by="integral", inplace=True)
-            all_df.sort_values(by="integral", inplace=True)
-            # print(all_df)
-            return plottables_df
-
-    stat_err_opts = {
-        "step": "post",
-        "label": "Stat. unc.",
-        "hatch": "//////",
-        "facecolor": "none",
-        "edgecolor": (0, 0, 0, 0.5),
-        "linewidth": 0,
-    }
-    ratio_err_opts = {"step": "post", "facecolor": (0, 0, 0, 0.3), "linewidth": 0}
 
     entry_types = ["stack", "step", "data"]
     entries = {et: Entry(et, parameters) for et in entry_types}
@@ -237,8 +132,6 @@ def plot(hist, parameters={}):
                     **stat_err_opts,
                 )
 
-    # ax1.set_xlim(var.xmin,var.xmax)
-    # ax1.set_xlim(edges[0], edges[-1])
     ax1.set_yscale("log")
     ax1.set_ylim(0.01, 1e9)
     ax1.legend(prop={"size": "x-small"})
@@ -250,9 +143,9 @@ def plot(hist, parameters={}):
         ax1.set_xlabel(var.label, loc="right")
 
     if parameters["plot_ratio"]:
+
         # Bottom panel: Data/MC ratio plot
         ax2 = fig.add_subplot(gs[1], sharex=ax1)
-
         num = den = []
 
         if len(entries["data"].entry_list) > 0:
