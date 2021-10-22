b673adfe4cb2015cd28625204020f65a62898ad3
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2021-10-22 13:53:53 -0400

    cleanup

--- python/utils.py
+++ python/utils.py
@@ -1,7 +1,4 @@
 import os
-import pandas as pd
-import dask.dataframe as dd
-import pickle
 
 
 def almost_equal(a, b):
@@ -13,60 +10,3 @@ def mkdir(path):
         os.mkdir(path)
     except Exception:
         pass
-
-
-def saving_func_parquet(output, out_dir):
-    from dask.distributed import get_worker
-
-    name = None
-    for key, task in get_worker().tasks.items():
-        if task.state == "executing":
-            name = key[-32:]
-    if not name:
-        return
-    for ds in output.s.unique():
-        df = output[output.s == ds]
-        if df.shape[0] == 0:
-            return
-        mkdir(f"{out_dir}/{ds}")
-        df.to_parquet(
-            path=f"{out_dir}/{ds}/{name}.parquet",
-        )
-
-
-def load_from_parquet(path):
-    df = dd.from_pandas(pd.DataFrame(), npartitions=1)
-    if len(path) > 0:
-        try:
-            df = dd.read_parquet(path)
-        except Exception:
-            return df
-    return df
-
-
-def save_hist(hist, var_name, dataset, year, parameters):
-    mkdir(parameters["hist_path"])
-    hist_path = parameters["hist_path"] + parameters["label"]
-    mkdir(hist_path)
-    mkdir(f"{hist_path}/{year}")
-    mkdir(f"{hist_path}/{year}/{var_name}")
-    path = f"{hist_path}/{year}/{var_name}/{dataset}.pickle"
-    with open(path, "wb") as handle:
-        pickle.dump(hist, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-
-def load_histograms(argset, parameters):
-    year = argset["year"]
-    var_name = argset["var_name"]
-    dataset = argset["dataset"]
-    hist_path = parameters["hist_path"] + parameters["label"]
-    path = f"{hist_path}/{year}/{var_name}/{dataset}.pickle"
-    try:
-        with open(path, "rb") as handle:
-            hist = pickle.load(handle)
-    except Exception:
-        return pd.DataFrame()
-    hist_row = pd.DataFrame(
-        [{"year": year, "var_name": var_name, "dataset": dataset, "hist": hist}]
-    )
-    return hist_row
