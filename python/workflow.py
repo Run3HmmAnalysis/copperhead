e59aaff37ace93944555515194b9ba7b1f1d9e0c
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2021-10-25 16:14:57 -0400

    cleanup

--- python/workflow.py
+++ python/workflow.py
@@ -2,28 +2,24 @@ import itertools
 from functools import partial
 
 
-def argset_product(arglist):
-    return [
-        dict(zip(arglist.keys(), values))
-        for values in itertools.product(*arglist.values())
-    ]
-
-
 def parallelize(
     func,
-    argsets,
+    argset,
     client,
     parameters={},
     timer=None,
 ):
     """
-    arg_exec is a dictionary of possible values of arguments for func_exec.
-    All combinations of parameters are computed, and func_exec is executed for each combination.
+    `argset` is a dictionary of possible values of arguments for `func`.
+    All combinations of parameters are computed, and `func` is executed for each combination.
 
     """
 
-    argsets = argset_product(argsets)
-    futures = client.map(partial(func, parameters=parameters), argsets)
+    argset = [
+        dict(zip(argset.keys(), values))
+        for values in itertools.product(*argset.values())
+    ]
+    futures = client.map(partial(func, parameters=parameters), argset)
     results = client.gather(futures)
 
     return results
