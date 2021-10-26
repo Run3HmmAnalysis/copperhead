b86b333953c4b7e29b63a5e72ce1d02f0e32054c
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2021-10-25 20:48:18 -0400

    merge files back for conciseness

--- python/plotter.py
+++ python/plotter.py
@@ -4,7 +4,6 @@ from hist.intervals import poisson_interval
 from python.workflow import parallelize
 from python.io import load_histogram
 from python.variable import Variable
-from python.entry import Entry
 
 import matplotlib.pyplot as plt
 import mplhep as hep
@@ -23,6 +22,43 @@ stat_err_opts = {
     "linewidth": 0,
 }
 ratio_err_opts = {"step": "post", "facecolor": (0, 0, 0, 0.3), "linewidth": 0}
+
+
+class Entry(object):
+    """
+    Different types of entries to put on the plot
+    """
+
+    def __init__(self, entry_type="step", parameters={}):
+        self.entry_type = entry_type
+        self.labels = []
+        if entry_type == "step":
+            self.histtype = "step"
+            self.stack = False
+            self.plot_opts = {"linewidth": 3}
+            self.yerr = False
+        elif entry_type == "stack":
+            self.histtype = "fill"
+            self.stack = True
+            self.plot_opts = {"alpha": 0.8, "edgecolor": (0, 0, 0)}
+            self.yerr = False
+        elif entry_type == "errorbar":
+            self.histtype = "errorbar"
+            self.stack = False
+            self.plot_opts = {"color": "k", "marker": ".", "markersize": 10}
+            self.yerr = True
+        else:
+            raise Exception(f"Wrong entry type: {entry_type}")
+
+        self.groups = parameters["plot_groups"][entry_type]
+        self.entry_dict = {
+            e: g
+            for e, g in parameters["grouping"].items()
+            if (g in self.groups) and (e in parameters["datasets"])
+        }
+        self.entry_list = self.entry_dict.keys()
+        self.labels = self.entry_dict.values()
+        self.groups = list(set(self.entry_dict.values()))
 
 
 def plotter(client, parameters, hist_df=None, timer=None):
