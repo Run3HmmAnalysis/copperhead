d192f72bf6209f8f3b3bb90d2a3f05ce5d82168e
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2022-03-24 14:07:57 -0400

    update tests

--- tests/test_delphes_continuous.py
+++ tests/test_delphes_continuous.py
@@ -124,7 +124,7 @@ if __name__ == "__main__":
     out_df = out_df.compute()
     dimuon_mass = out_df.loc[out_df.event == 20002, "dimuon_mass"].values[0]
     jj_mass = out_df.loc[out_df.event == 20011, "jj_mass"].values[0]
-    assert out_df.shape == (86, 63)
+    assert out_df.shape == (86, 78)
     assert almost_equal(dimuon_mass, 124.3369651)
     assert almost_equal(jj_mass, 78.593476)
     slicer = {
