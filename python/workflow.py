5914d1de6889329cdc6b171690053fcad3a1e2f8
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2022-03-29 18:07:53 -0400

    option to run sequentially

--- python/workflow.py
+++ python/workflow.py
@@ -2,7 +2,7 @@ import itertools
 from functools import partial
 
 
-def parallelize(func, argset, client, parameters={}, timer=None):
+def parallelize(func, argset, client, parameters={}, timer=None, seq=False):
     """
     `argset` is a dictionary of possible values of arguments for `func`.
     All combinations of parameters are computed, and `func` is executed for each combination.
@@ -13,8 +13,13 @@ def parallelize(func, argset, client, parameters={}, timer=None):
         dict(zip(argset.keys(), values))
         for values in itertools.product(*argset.values())
     ]
-    map_futures = client.scatter(argset)
-    futures = client.map(partial(func, parameters=parameters), map_futures)
-    results = client.gather(futures)
+    if seq:
+        results = []
+        for args in argset:
+            results.append(func(args, parameters))
+    else:
+        map_futures = client.scatter(argset)
+        futures = client.map(partial(func, parameters=parameters), map_futures)
+        results = client.gather(futures)
 
     return results
