7d29e9f126628f660e835ad6b182a78fcae04d7e
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2022-04-10 18:16:03 -0400

    remove duplicate code

--- stage2/convert.py
+++ stage2/convert.py
@@ -1,40 +1,17 @@
-import itertools
 import numpy as np
 import pandas as pd
-from hist import Hist
-import dask.dataframe as dd
 
 from python.workflow import parallelize
 from python.variable import Variable
 from python.io import (
     load_histogram,
-    save_histogram,
     save_template,
-    delete_existing_hists,
 )
-from stage2.categorizer import split_into_channels
 
 import warnings
 
 warnings.simplefilter(action="ignore", category=FutureWarning)
 from uproot3_methods.classes.TH1 import from_numpy
-
-
-def to_histograms(client, parameters, df):
-    argset = {
-        "year": df.year.unique(),
-        "var_name": parameters["hist_vars"],
-        "dataset": df.dataset.unique(),
-    }
-    if isinstance(df, pd.DataFrame):
-        argset["df"] = [df]
-    elif isinstance(df, dd.DataFrame):
-        argset["df"] = [(i, df.partitions[i]) for i in range(df.npartitions)]
-
-    parallelize(delete_existing_hists, argset, client, parameters, seq=True)
-    hist_rows = parallelize(make_histograms, argset, client, parameters)
-    hist_df = pd.concat(hist_rows).reset_index(drop=True)
-    return hist_df
 
 
 def to_templates(client, parameters, hist_df=None):
@@ -58,153 +35,6 @@ def to_templates(client, parameters, hist_df=None):
     }
     yields = parallelize(make_templates, argset, client, parameters)
     return yields
-
-
-def get_variation(wgt_variation, sys_variation):
-    if "nominal" in wgt_variation:
-        if "nominal" in sys_variation:
-            return "nominal"
-        else:
-            return sys_variation
-    else:
-        if "nominal" in sys_variation:
-            return wgt_variation
-        else:
-            return None
-
-
-def make_histograms(args, parameters={}):
-    df = args["df"]
-    npart = None
-    if isinstance(df, tuple):
-        npart = df[0]
-        df = df[1]
-
-    year = args["year"]
-    var_name = args["var_name"]
-    dataset = args["dataset"]
-    if var_name in parameters["variables_lookup"].keys():
-        var = parameters["variables_lookup"][var_name]
-    else:
-        var = Variable(var_name, var_name, 50, 0, 5)
-
-    regions = parameters["regions"]
-    channels = parameters["channels"]
-
-    if isinstance(df, dd.DataFrame):
-        df = df.compute()
-    df.fillna(-999.0, inplace=True)
-
-    if "dy_m105_160_vbf_amc" in dataset:
-        df = df[df.gjj_mass > 350]
-    if "dy_m105_160_amc" in dataset:
-        df = df[df.gjj_mass <= 350]
-
-    try:
-        split_into_channels(df, v="nominal")
-    except Exception:
-        split_into_channels(df)
-
-    wgt_variations = ["nominal"]
-    syst_variations = ["nominal"]
-    variations = []
-    if parameters["has_variations"]:
-        c_name = "channel nominal"
-        wgt_variations = [w for w in df.columns if ("wgt_" in w)]
-        syst_variations = parameters["syst_variations"]
-
-        for w in wgt_variations:
-            for v in syst_variations:
-                variation = get_variation(w, v)
-                if variation:
-                    variations.append(variation)
-    else:
-        c_name = "channel"
-
-    regions = [r for r in regions if r in df.region.unique()]
-    channels = [c for c in channels if c in df[c_name].unique()]
-
-    # prepare multidimensional histogram
-    hist = (
-        Hist.new.StrCat(regions, name="region")
-        .StrCat(channels, name="channel")
-        .StrCat(["value", "sumw2"], name="val_sumw2")
-    )
-
-    # axis for observable variable
-    if ("score" in var.name) and ("mva_bins" in parameters.keys()):
-        bins = parameters["mva_bins"][var.name.replace("score_", "")][f"{year}"]
-        hist = hist.Var(bins, name=var.name)
-    else:
-        hist = hist.Reg(var.nbins, var.xmin, var.xmax, name=var.name, label=var.caption)
-
-    # axis for systematic variation
-    if parameters["has_variations"]:
-        hist = hist.StrCat(variations, name="variation")
-
-    # container type
-    hist = hist.Double()
-
-    loop_args = {
-        "region": regions,
-        "w": wgt_variations,
-        "v": syst_variations,
-        "channel": channels,
-    }
-    loop_args = [
-        dict(zip(loop_args.keys(), values))
-        for values in itertools.product(*loop_args.values())
-    ]
-    for loop_arg in loop_args:
-        region = loop_arg["region"]
-        channel = loop_arg["channel"]
-        w = loop_arg["w"]
-        v = loop_arg["v"]
-        variation = get_variation(w, v)
-        if not variation:
-            continue
-        if parameters["has_variations"]:
-            var_name = f"{var.name} {v}"
-            ch_name = f"channel {v}"
-            if var_name not in df.columns:
-                if var.name in df.columns:
-                    var_name = var.name
-                else:
-                    continue
-        else:
-            var_name = var.name
-            ch_name = "channel"
-
-        slicer = (
-            (df.dataset == dataset)
-            & (df.region == region)
-            & (df.year == year)
-            & (df[ch_name] == channel)
-        )
-        data = df.loc[slicer, var_name]
-
-        to_fill = {var.name: data, "region": region, "channel": channel}
-        to_fill_value = to_fill.copy()
-        to_fill_sumw2 = to_fill.copy()
-        to_fill_value["val_sumw2"] = "value"
-        to_fill_sumw2["val_sumw2"] = "sumw2"
-
-        if parameters["has_variations"]:
-            to_fill_value["variation"] = variation
-            to_fill_sumw2["variation"] = variation
-            weight = df.loc[slicer, w]
-        else:
-            weight = df.loc[slicer, "lumi_wgt"]  # * df.loc[slicer, "mc_wgt"]
-
-        hist.fill(**to_fill_value, weight=weight)
-        hist.fill(**to_fill_sumw2, weight=weight * weight)
-
-    if parameters["save_hists"]:
-        save_histogram(hist, var.name, dataset, year, parameters, npart)
-    hist_row = pd.DataFrame(
-        [{"year": year, "var_name": var.name, "dataset": dataset, "hist": hist}]
-    )
-    return hist_row
 
 
 def make_templates(args, parameters={}):
