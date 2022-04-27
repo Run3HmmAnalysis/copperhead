2da141bd923033287111d571b34374d09cd2aada
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2022-04-27 13:45:46 -0400

    fix tests

--- tests/test_stage2.py
+++ tests/test_stage2.py
@@ -18,8 +18,8 @@ __all__ = ["dask"]
 parameters = {
     "ncpus": 1,
     "years": [2016],
-    "datasets": ["dy_m105_160_amc"],
-    "channels": ["vbf"],
+    "datasets": ["data_B"],
+    "channels": ["ggh_0jets"],
     "regions": ["h-peak"],
     "hist_vars": ["dimuon_mass"],
     "variables_lookup": variables_lookup,
@@ -33,7 +33,7 @@ if __name__ == "__main__":
         processes=True, n_workers=1, threads_per_worker=1, memory_limit="4GB"
     )
 
-    file_name = "dy_nanoaod_stage1_output.parquet"
+    file_name = "data_B_stage1_output.parquet"
     path = f"{os.getcwd()}/tests/samples/{file_name}"
 
     df = load_dataframe(client, parameters, inputs=[path])
@@ -41,8 +41,8 @@ if __name__ == "__main__":
 
     elapsed = round(time.time() - tick, 3)
     print(f"Finished everything in {elapsed} s.")
-
+    print(out_hist)
     assert almost_equal(
         out_hist.loc[out_hist.variation == "nominal", "yield"].values[0],
-        0.14842246076249055,
+        21.0,
     )
