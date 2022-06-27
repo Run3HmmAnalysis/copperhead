6e3fba1bb38cc2f2b4600a6ff58a90757bd58606
Dmitry Kondratyev <kondratyev.d.95@gmail.com>
2022-06-27 07:41:15 -0400

    bins for vbf channel synchronized with Run2

--- stage2/categorizer.py
+++ stage2/categorizer.py
@@ -47,3 +47,77 @@ def categorize_by_score(df, scores, mode="uniform", **kwargs):
                 cut_hi = score.quantile((i + 1) / nbins)
                 cut = (df.channel == channel) & (score > cut_lo) & (score < cut_hi)
                 df.loc[cut, "category"] = cat_name
+
+
+def categorize_dnn_output(df, score_name, channel, region, year):
+    target_yields = {
+        "2016": [
+            0.35455259,
+            0.50239086,
+            0.51152889,
+            0.52135985,
+            0.5282209,
+            0.54285134,
+            0.54923751,
+            0.56504687,
+            0.57204477,
+            0.58273066,
+            0.5862248,
+            0.59568793,
+            0.60871905,
+        ],
+        "2017": [
+            0.44194544,
+            0.55885064,
+            0.57796123,
+            0.58007343,
+            0.58141001,
+            0.58144682,
+            0.57858609,
+            0.59887533,
+            0.59511901,
+            0.59644831,
+            0.59825915,
+            0.59283309,
+            0.57329743,
+        ],
+        "2018": [
+            0.22036263,
+            1.31808978,
+            1.25396849,
+            1.183724,
+            1.12620194,
+            1.06041376,
+            0.99941623,
+            0.93224412,
+            0.87074753,
+            0.80599462,
+            0.73469265,
+            0.668018,
+            0.60991945,
+        ],
+    }
+
+    bins = [df[score_name].max()]
+
+    slicer = (
+        (df.channel_nominal == channel) & (df.region == region) & (df.year == int(year))
+    )
+    df_sorted = (
+        df.loc[slicer, :]
+        .sort_values(by=score_name, ascending=False)
+        .reset_index(drop=True)
+    )
+    df_sorted["wgt_cumsum"] = df_sorted.wgt_nominal.cumsum()
+
+    tot_yield = 0
+    for yi in reversed(target_yields[year]):
+        tot_yield += yi
+        for i in range(df_sorted.shape[0] - 1):
+            value = df_sorted.loc[i, "wgt_cumsum"]
+            value1 = df_sorted.loc[i + 1, "wgt_cumsum"]
+            if (value < tot_yield) & (value1 > tot_yield):
+                bins.append(df_sorted.loc[i, score_name])
+    bins.append(0.0)
+    bins = sorted(bins)
+    print(bins)
