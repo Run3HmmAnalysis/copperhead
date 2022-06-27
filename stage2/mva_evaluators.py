add Pisa DNN architecture; fix batch normalization

stage2/mva_evaluators.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pickle
 import torch
-from stage2.mva_models import Net
+from stage2.mva_models import Net, NetPisaRun2, NetPisaRun2Combination
 
 
 training_features = [
@@ -9,10 +9,14 @@ training_features = [
     "dimuon_pt",
     "dimuon_pt_log",
     "dimuon_eta",
-    "dimuon_ebe_mass_res",
-    "dimuon_ebe_mass_res_rel",
-    "dimuon_cos_theta_cs",
-    "dimuon_phi_cs",
+    # "dimuon_ebe_mass_res",
+    # "dimuon_ebe_mass_res_rel",
+    # "dimuon_cos_theta_cs",
+    # "dimuon_phi_cs",
+    "dimuon_pisa_mass_res",
+    "dimuon_pisa_mass_res_rel",
+    "dimuon_cos_theta_cs_pisa",
+    "dimuon_phi_cs_pisa",
     "jet1_pt",
     "jet1_eta",
     "jet1_phi",
@@ -29,6 +33,38 @@ training_features = [
     "mmj_min_dEta",
     "nsoftjets5",
     "htsoft2",
+]
+
+
+training_features_mass = [
+    "dimuon_mass",
+    "dimuon_pisa_mass_res",
+    "dimuon_pisa_mass_res_rel",
+]
+
+training_features_nomass = [
+    "dimuon_pt",
+    "dimuon_pt_log",
+    "dimuon_eta",
+    "dimuon_cos_theta_cs_pisa",
+    "dimuon_phi_cs_pisa",
+    "jet1_pt_nominal",
+    "jet1_eta_nominal",
+    "jet1_phi_nominal",
+    "jet1_qgl_nominal",
+    "jet2_pt_nominal",
+    "jet2_eta_nominal",
+    "jet2_phi_nominal",
+    "jet2_qgl_nominal",
+    "jj_mass_nominal",
+    "jj_mass_log_nominal",
+    "jj_dEta_nominal",
+    "rpt_nominal",
+    "ll_zstar_log_nominal",
+    "mmj_min_dEta_nominal",
+    "nsoftjets5_nominal",
+    "htsoft2_nominal",
+    "year",
 ]
 
 
@@ -50,7 +86,8 @@ def prepare_features(df, parameters, variation="nominal", add_year=False):
 
 
 def evaluate_pytorch_dnn(df, variation, model, parameters, score_name, channel):
-    features = prepare_features(df, parameters, variation)
+    features = prepare_features(df, parameters, variation, add_year=True)
+
     try:
         df = df.compute()
     except Exception:
@@ -81,12 +118,142 @@ def evaluate_pytorch_dnn(df, variation, model, parameters, score_name, channel):
         df_i = torch.tensor(df_i.values).float()
 
         dnn_model = Net()
-        model_path = (
-            f"{parameters['models_path']}/{channel}/models/model_{model}_{i}.pt"
+        model_path = f"{parameters['models_path']}/{channel}/models/{model}_{i}.pt"
+        dnn_model.load_state_dict(
+            torch.load(model_path, map_location=torch.device("cpu"))
         )
-        dnn_model.load_state_dict(torch.load(model_path))
         dnn_model.eval()
+
+        """
+        output = dnn_model.pre_output(df_i).detach().numpy()
+        import pandas as pd
+        print(pd.DataFrame(output))
+        print(pd.DataFrame(output).value_counts())
+        print(pd.DataFrame(output).value_counts().values.max())
+        #print(dnn_model(df_i).detach().numpy()[0] - 0.9173)
+        import sys
+        sys.exit()
+        """
         df.loc[eval_filter, score_name] = np.arctanh((dnn_model(df_i).detach().numpy()))
+
+    return df[score_name]
+
+
+def evaluate_pytorch_dnn_pisa(
+    df, variation, model_name, parameters, score_name, channel
+):
+    features = prepare_features(df, parameters, variation, add_year=True)
+
+    try:
+        df = df.compute()
+    except Exception:
+        pass
+
+    if df.shape[0] == 0:
+        return None
+
+    df.loc[:, score_name] = 0
+
+    nfolds = 4
+    for i in range(nfolds):
+        # train_folds = [(i + f) % nfolds for f in [0, 1]]
+        # val_folds = [(i + f) % nfolds for f in [2]]
+        eval_folds = [(i + f) % nfolds for f in [3]]
+
+        eval_filter = df.event.mod(nfolds).isin(eval_folds)
+
+        scalers_path = f"{parameters['models_path']}/{channel}/scalers/scalers_{model_name}_{i}.npy"
+        scalers = np.load(scalers_path)
+        df_i = df.loc[eval_filter, :]
+        if df_i.shape[0] == 0:
+            continue
+
+        df_i.loc[df_i.region != "h-peak", "dimuon_mass"] = 125.0
+
+        df_i = (df_i[features] - scalers[0]) / scalers[1]
+        # df_i = torch.tensor(df_i.values).float()
+        df_i_mass = df_i[training_features_mass]
+        df_i_nomass = df_i[training_features_nomass]
+        df_i_mass = torch.tensor(df_i_mass.values).float()
+        df_i_nomass = torch.tensor(df_i_nomass.values).float()
+
+        nlayers = 3
+        nnodes = [64, 32, 16]
+        freeze = []
+
+        training_setup = {
+            "sig_vs_ewk": {
+                "datasets": [
+                    "ewk_lljj_mll105_160_py_dipole",
+                    "ggh_amcPS",
+                    "vbf_powheg_dipole",
+                ],
+                "features": training_features_mass + training_features_nomass,
+            },
+            "sig_vs_dy": {
+                "datasets": [
+                    "dy_m105_160_amc",
+                    "dy_m105_160_vbf_amc",
+                    "ggh_amcPS",
+                    "vbf_powheg_dipole",
+                ],
+                "features": training_features_mass + training_features_nomass,
+            },
+            "no_mass": {
+                "datasets": [
+                    "dy_m105_160_amc",
+                    "dy_m105_160_vbf_amc",
+                    "ttjets_dl",
+                    "ggh_amcPS",
+                    "vbf_powheg_dipole",
+                    "ewk_lljj_mll105_160_py_dipole",
+                ],
+                "features": training_features_nomass,
+            },
+            "mass": {
+                "datasets": [
+                    "dy_m105_160_amc",
+                    "dy_m105_160_vbf_amc",
+                    "ttjets_dl",
+                    "ggh_amcPS",
+                    "vbf_powheg_dipole",
+                    "ewk_lljj_mll105_160_py_dipole",
+                ],
+                "features": training_features_mass,
+            },
+            "combination": {
+                "datasets": [
+                    "dy_m105_160_amc",
+                    "dy_m105_160_vbf_amc",
+                    "ttjets_dl",
+                    "ggh_amcPS",
+                    "vbf_powheg_dipole",
+                    "ewk_lljj_mll105_160_py_dipole",
+                ],
+            },
+        }
+        subnetworks = {}
+        for name in ["sig_vs_ewk", "sig_vs_dy", "no_mass", "mass"]:
+            subnetworks[name] = NetPisaRun2(
+                name, len(training_setup[name]["features"]), nlayers, nnodes
+            )
+            # subnetworks[name].to(device)
+            model_path = f"data/trained_models/vbf/models/{model_name}_{name}_{i}.pt"
+            subnetworks[name].load_state_dict(
+                torch.load(model_path, map_location=torch.device("cpu"))
+            )
+        dnn_model = NetPisaRun2Combination(
+            "combination", nlayers, nnodes, subnetworks, freeze
+        )
+
+        model_path = f"{parameters['models_path']}/{channel}/models/{model_name}_combination_{i}.pt"
+        dnn_model.load_state_dict(
+            torch.load(model_path, map_location=torch.device("cpu"))
+        )
+        dnn_model.eval()
+        df.loc[eval_filter, score_name] = np.arctanh(
+            (dnn_model(df_i_nomass, df_i_mass).detach().numpy())
+        )
 
     return df[score_name]
 

stage2/mva_models.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -5,28 +6,108 @@ import torch.nn.functional as F
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.fc1 = nn.Linear(24, 128)
+        self.fc1 = nn.Linear(25, 128)
+        self.bn1 = nn.BatchNorm1d(128)
         self.dropout1 = nn.Dropout(0.2)
         self.fc2 = nn.Linear(128, 64)
+        self.bn2 = nn.BatchNorm1d(64)
         self.dropout2 = nn.Dropout(0.2)
         self.fc3 = nn.Linear(64, 32)
+        self.bn3 = nn.BatchNorm1d(32)
         self.dropout3 = nn.Dropout(0.2)
         self.output = nn.Linear(32, 1)
 
     def forward(self, x):
-        # Pass data through conv1
         x = self.fc1(x)
+        x = self.bn1(x)
         x = F.tanh(x)
         x = self.dropout1(x)
 
         x = self.fc2(x)
+        x = self.bn2(x)
         x = F.tanh(x)
         x = self.dropout2(x)
 
         x = self.fc3(x)
+        x = self.bn3(x)
         x = F.tanh(x)
         x = self.dropout3(x)
 
         x = self.output(x)
         output = F.sigmoid(x)
         return output
+
+
+class NetPisaRun2(nn.Module):
+    def __init__(self, name, input_dim, nlayers, nnodes):
+        super(NetPisaRun2, self).__init__()
+
+        assert nlayers == len(nnodes)
+        self.nlayers = nlayers
+        self.name = name
+        inp_shape = input_dim
+        self.layers = {}
+        for i in range(self.nlayers):
+            out_shape = nnodes[i]
+            self.layers[f"{name}_layer_{i}"] = nn.Linear(inp_shape, out_shape)
+            self.layers[f"{name}_dropout_{i}"] = nn.Dropout(0.2)
+            inp_shape = out_shape
+
+        self.layers[f"{name}_output"] = nn.Linear(inp_shape, 1)
+        self.layers = nn.ModuleDict(self.layers)
+
+    def forward(self, x):
+        for i in range(self.nlayers):
+            x = self.layers[f"{self.name}_layer_{i}"](x)
+            x = F.tanh(x)
+            x = self.layers[f"{self.name}_dropout_{i}"](x)
+        x = self.layers[f"{self.name}_output"](x)
+        x = F.sigmoid(x)
+        return x
+
+
+class NetPisaRun2Combination(nn.Module):
+    def __init__(self, name, nlayers, nnodes, subnetworks, freeze):
+        super(NetPisaRun2Combination, self).__init__()
+        assert nlayers == len(nnodes)
+        self.subnetworks = subnetworks
+        self.freeze = freeze
+        self.nlayers = nlayers
+        self.name = name
+        inp_shape = 4
+        self.layers = {}
+        for i in range(self.nlayers):
+            out_shape = nnodes[i]
+            self.layers[f"{name}_layer_{i}"] = nn.Linear(inp_shape, out_shape)
+            self.layers[f"{name}_dropout_{i}"] = nn.Dropout(0.2)
+            inp_shape = out_shape
+
+        self.layers[f"{name}_output"] = nn.Linear(inp_shape, 1)
+        self.layers = nn.ModuleDict(self.layers)
+
+    def forward(self, inp_nomass, inp_mass):
+        all_inputs = torch.cat((inp_mass, inp_nomass), 1)
+        for key in self.subnetworks.keys():
+            if key in self.freeze:
+                for param in self.subnetworks[key].parameters():
+                    param.requires_grad = False
+            else:
+                for param in self.subnetworks[key].parameters():
+                    param.requires_grad = True
+
+        self.output1 = self.subnetworks["sig_vs_ewk"](all_inputs)
+        self.output2 = self.subnetworks["sig_vs_dy"](all_inputs)
+        self.output3 = self.subnetworks["no_mass"](inp_nomass)
+        self.output4 = self.subnetworks["mass"](inp_mass)
+        self.combine_input = torch.cat(
+            (self.output1, self.output2, self.output3, self.output4), 1
+        )
+        x = self.combine_input
+        for i in range(self.nlayers):
+            x = self.layers[f"{self.name}_layer_{i}"](x)
+            x = F.tanh(x)
+            x = self.layers[f"{self.name}_dropout_{i}"](x)
+        x = self.layers[f"{self.name}_output"](x)
+        x = F.sigmoid(x)
+
+        return x